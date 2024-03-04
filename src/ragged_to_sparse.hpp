0517c49f7cfc01cdd666ab886de026a3917448f9
Artur Paniukov <artur.paniukov@intel.com>
2024-03-04 21:19:03 +0400

    Add RaggedToSparse Op (#49)
    Add RaggedToSparse Operation
    
    
    Co-authored-by: Roman Kazantsev <roman.kazantsev@intel.com>

---
+++ src/ragged_to_sparse.hpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <openvino/op/op.hpp>
+
+// Takes one ragged dimension (starts, ends) and produces 2D tensor of sparse coordinates [(row, col), ...]
+class RaggedToSparse : public ov::op::Op {
+public:
+    OPENVINO_OP("RaggedToSparse");
+
+    RaggedToSparse () = default;
+
+    RaggedToSparse(const ov::OutputVector& arguments) :
+        ov::op::Op(arguments) {
+        constructor_validate_and_infer_types();
+    }
+
+    void validate_and_infer_types() override;
+
+    std::shared_ptr<ov::Node> clone_with_new_inputs(const ov::OutputVector& inputs) const override {
+        return std::make_shared<RaggedToSparse>(inputs);
+    }
+
+    bool visit_attributes(ov::AttributeVisitor& visitor) override {
+        return true;
+    }
+
+    bool evaluate(ov::TensorVector& outputs, const ov::TensorVector& inputs) const override;
+
+    bool has_evaluate() const override {
+        return true;
+    }
+};
