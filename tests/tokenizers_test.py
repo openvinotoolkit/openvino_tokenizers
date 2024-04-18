f68414e8ea96c634f275a582495f19f140cd42de
Artur Paniukov <chgk1101@gmail.com>
2024-04-18 20:19:45 +0100

    Add llama3 tokenizer to tests

--- tests/tokenizers_test.py
+++ tests/tokenizers_test.py
@@ -86,6 +86,7 @@ wordpiece_models = [
     "rasa/LaBSE",
 ]
 bpe_models = [
+    "meta-llama/Meta-Llama-3-8B",
     "stabilityai/stablecode-completion-alpha-3b-4k",
     "stabilityai/stablelm-tuned-alpha-7b",
     "databricks/dolly-v2-3b",
