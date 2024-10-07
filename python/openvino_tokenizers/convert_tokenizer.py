e89a612f65083f393fdbf76149de450afb341438
Pavel Esir <pavel.esir@intel.com>
2024-10-07 23:46:48 +0200

    fix tests 3

--- python/openvino_tokenizers/convert_tokenizer.py
+++ python/openvino_tokenizers/convert_tokenizer.py
@@ -114,7 +114,7 @@ def convert_tokenizer(
     can_use_sentencepiece = is_sentencepiece_model(tokenizer_object)
     is_unigram = can_use_sentencepiece and not is_sentencepiece_bpe_model(tokenizer_object)
     if isinstance(tokenizer_object, PreTrainedTokenizerBase):
-        if can_use_sentencepiece and (is_unigram or not tokenizer_object.is_fast or use_sentencepiece_backend):
+        if can_use_sentencepiece and (is_unigram or not tokenizer_object.is_fast or params.use_sentencepiece_backend):
             logger.info("Convert tokenizer using SentencePiece .model file.")
             ov_tokenizers = convert_sentencepiece_model_tokenizer(tokenizer_object, params)
         elif is_tiktoken_model(tokenizer_object):
