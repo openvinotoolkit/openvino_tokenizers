97c252cd0f2867cf6cae91c1c8db904eda5fedac
Pavel Esir <pavel.esir@intel.com>
2024-11-21 12:46:03 +0100

    add VocabDecode with input skip_tokens

--- python/openvino_tokenizers/tokenizer_pipeline.py
+++ python/openvino_tokenizers/tokenizer_pipeline.py
@@ -14,7 +14,7 @@ from itertools import groupby, islice
 from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
 
 import numpy as np
-from openvino.runtime import Model, Output, PartialShape, Type, op
+from openvino.runtime import Model, Output, PartialShape, Type, op, Shape
 from openvino.runtime import opset12 as opset
 from openvino.runtime.exceptions import OVTypeError, UserInputError
 from openvino.runtime.utils.types import as_node, make_constant_node
@@ -1025,7 +1025,8 @@ class VocabDecoderStep(DecodingStep):
         else:
             vocab_outputs = self.create_string_constant_node(self.vocab).outputs()
         input_nodes.extend(vocab_outputs)
-        return _get_factory().create("VocabDecoder", input_nodes, {"skip_tokens": self.skip_tokens}).outputs()
+        input_nodes.extend(op.Constant(Type.i32, Shape([len(self.skip_tokens)]), self.skip_tokens).outputs())
+        return _get_factory().create("VocabDecoder", input_nodes).outputs()
 
 
 @dataclass
