ac5c3978337cd74594da6e7bf687af02bb8aa959
Pavel Esir <pavel.esir@intel.com>
2025-01-15 22:12:40 +0100

    Use opset15 version of Str Pack/Unpack (#351)
    * Use String Pack/Unpack from opset
    
    * adjustments for opset15 string
    
    * Update python/openvino_tokenizers/tokenizer_pipeline.py
    
    * apply comments
    
    * new pass rates
    
    * use BytesIO for performance on large vocabs, some other updates
    
    * Apply suggestions from code review
    
    * update stats
    
    * fix tests
    
    * update tests statistics
    
    * regex_split fasttrack for empty tensors
    
    * fix w/a for empty tensors
    
    * update stats

--- python/openvino_tokenizers/utils.py
+++ python/openvino_tokenizers/utils.py
@@ -6,11 +6,17 @@ import logging
 import re
 from dataclasses import dataclass, field, fields
 from functools import lru_cache
-from typing import Any, Dict, Optional, Sequence, Tuple, Union
+from typing import Any, Dict, Optional, Sequence, Tuple, Union, Iterable, List
+import numpy as np
+from numpy.typing import NDArray
+from io import BytesIO
+
 
 from openvino import Model, Type
 from openvino.preprocess import PrePostProcessor
-from openvino.runtime import opset12 as opset
+from openvino.runtime import opset12 as opset, Output
+from openvino.op import Constant
+from openvino import Tensor
 
 from .constants import (
     LOGITS_OUTPUT_NAME,
@@ -20,7 +26,6 @@ from .constants import (
     UTF8ReplaceMode,
     rt_info_to_hf_attribute_map,
 )
-
 
 @dataclass
 class TokenzierConversionParams:
@@ -295,3 +300,32 @@ def quote_meta(unquoted: Union[str, bytes]) -> str:
             symbols.append("\\")
         symbols.append(char)
     return "".join(symbols)
+
+
+def to_bytes(number: int) -> bytes:
+    return number.to_bytes(4, "little")
+
+
+def create_unpacked_string(strings: Iterable[str]) -> List[Output]:
+    """
+    Convert any list of strings to U8/1D numpy array with begins, ends, and chars
+    """
+    begins = BytesIO()
+    ends = BytesIO()
+    chars = BytesIO()
+    offset = 0
+
+    for string in strings:
+        byte_string = string.encode("utf-8") if isinstance(string, str) else string
+        length = len(byte_string)
+
+        begins.write(to_bytes(offset))
+        offset += length
+        ends.write(to_bytes(offset))
+        chars.write(byte_string)
+
+    begins = np.frombuffer(begins.getvalue(), np.int32)
+    ends = np.frombuffer(ends.getvalue(), np.int32)
+    chars = np.frombuffer(chars.getvalue(), np.uint8)
+    
+    return [Constant(Tensor(x)).output(0) for x in [begins, ends, chars]]
--- src/regex_split.cpp
+++ src/regex_split.cpp
@@ -122,7 +122,29 @@ void RegexSplit::validate_and_infer_types() {
 }
 
 bool RegexSplit::evaluate(ov::TensorVector& outputs, const ov::TensorVector& inputs) const {
-    auto split_pattern = std::string(inputs[5].data<const char>(), inputs[5].get_size());
+    const size_t num_chars = inputs[4].get_size();
+    auto input_size = get_input_size();
+    const bool has_skips = (input_size == 7);
+
+    if (num_chars == 0) {
+        // TODO: CVS-160624. This is a workaround for the case when the input is empty.
+        outputs[0].set_shape(Shape{1});
+        outputs[1].set_shape(Shape{1});
+        outputs[0].data<int32_t>()[0] = 0;
+        outputs[1].data<int32_t>()[0] = 0;
+
+        outputs[2] = inputs[2];
+        outputs[3] = inputs[3];
+        outputs[4] = inputs[4];
+        if (has_skips) {
+            outputs[5] = inputs[5];
+        }
+        return true;
+    }
+
+    std::string split_pattern = std::string(inputs[5 + has_skips].data<const char>(), inputs[5 + has_skips].get_size());
+    auto pattern_size = inputs[5 + has_skips].get_size();
+    
     // Write to common trie structures should be protected to prevent race conditions.
     {
         std::lock_guard<std::mutex> lock(m_mutex);
@@ -138,7 +160,6 @@ bool RegexSplit::evaluate(ov::TensorVector& outputs, const ov::TensorVector& inp
         }
     };
 
-    auto input_size = get_input_size();
     {
         // Write to common trie structures should be protected to prevent race conditions.
         std::lock_guard<std::mutex> lock(m_mutex);
@@ -162,14 +183,10 @@ bool RegexSplit::evaluate(ov::TensorVector& outputs, const ov::TensorVector& inp
     auto begins = inputs[2].data<const int32_t>();
     auto ends   = inputs[3].data<const int32_t>();
     auto chars  = inputs[4].data<const uint8_t>();
-
-    outputs[4] = inputs[4];
+    
     const size_t num_rows = inputs[0].get_size();
-    const size_t num_chars = inputs[4].get_size();
-
     bool * skips;
     bool init_skips = false;
-    const bool has_skips = (input_size == 7);
     if (has_skips) {
         skips = inputs[5].data<bool>();
         outputs[5].set_shape(Shape{num_chars});
@@ -181,11 +198,10 @@ bool RegexSplit::evaluate(ov::TensorVector& outputs, const ov::TensorVector& inp
 
     outputs[0].set_shape(inputs[0].get_shape());
     outputs[1].set_shape(inputs[1].get_shape());
+    outputs[4] = inputs[4];
 
     outputs[2].set_shape(Shape{num_chars});
     outputs[3].set_shape(Shape{num_chars});
-
-    outputs[4] = inputs[4];
 
     // Get pointers in the output tensors
     auto new_ragged_begins = outputs[0].data<int32_t>();
@@ -301,5 +317,6 @@ bool RegexSplit::evaluate(ov::TensorVector& outputs, const ov::TensorVector& inp
         delete[] skips;
         delete[] new_skips;
     };
+
     return true;
 }
