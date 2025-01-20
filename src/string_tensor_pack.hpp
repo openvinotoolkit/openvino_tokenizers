afaa5217f24469842e5d86c7198358428665c53c
Roman Kazantsev <roman.kazantsev@intel.com>
2025-01-20 10:36:22 +0400

    Revert "Revert "Revert "Use opset15 version of Str Pack/Unpack (#351)" (#374)…" (#383)
    This reverts commit 2e59c9611330691d459a58357b839af7c3d3bc82.

--- src/string_tensor_pack.hpp
+++ src/string_tensor_pack.hpp
@@ -5,24 +5,20 @@
 #pragma once
 
 #include <openvino/op/op.hpp>
-#include <openvino/op/string_tensor_pack.hpp>
 
 // Having a decomposed representation for a tensor, converts it to a single string tensor with element::string element type.
-class StringTensorPack : public ov::op::v15::StringTensorPack {
+class StringTensorPack : public ov::op::Op {
 public:
-    OPENVINO_OP("StringTensorPack", "extension", ov::op::v15::StringTensorPack);
+    OPENVINO_OP("StringTensorPack");
 
     StringTensorPack () = default;
 
     StringTensorPack(ov::OutputVector inputs, const std::string& mode = "begins_ends")
-        : ov::op::v15::StringTensorPack(inputs[0], inputs[1], inputs[2]), m_mode(mode) {
+        : ov::op::Op(inputs), m_mode(mode) {
         constructor_validate_and_infer_types();
     }
 
-    void validate_and_infer_types() override {
-        OPENVINO_ASSERT(m_mode == "begins_ends", "StringTensorPack supports only 'begins_ends' mode, but get ", m_mode);
-        ov::op::v15::StringTensorPack::validate_and_infer_types();
-    }
+    void validate_and_infer_types() override;
 
     std::shared_ptr<ov::Node> clone_with_new_inputs(const ov::OutputVector& inputs) const override {
         auto result = std::make_shared<StringTensorPack>(inputs, m_mode);
@@ -33,6 +29,12 @@ public:
         visitor.on_attribute("mode", m_mode);
         return true;
     }
+
+    bool has_evaluate() const override {
+        return true;
+    }
+
+    bool evaluate(ov::TensorVector& outputs, const ov::TensorVector& inputs) const override;
 
 private:
 
