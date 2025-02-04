02a39bc86315005968f1aaa82dc0f7946acf8f30
Mikhail Ryzhov <mikhail.ryzhov@intel.com>
2025-02-04 08:52:16 +0100

    Revert "Revert "Update Normalization: attempt 2 (#394)" (#395)"
    This reverts commit 0561f499f746951522a21ec1a23b18c53bd04d31.

--- src/charsmap_normalization.hpp
+++ src/charsmap_normalization.hpp
@@ -26,19 +26,47 @@ public:
         const ov::OutputVector& arguments,
         const std::shared_ptr<sentencepiece::normalizer::Normalizer> normalizer,
         const std::shared_ptr<sentencepiece::NormalizerSpec> spec
-    ): ov::op::Op(arguments), m_normalizer(normalizer), m_spec(spec) {
+    ): ov::op::Op(arguments), m_normalizer(normalizer), m_spec(spec), m_init_flag() {
+        sentencepiece::logging::SetMinLogLevel(1);
+        constructor_validate_and_infer_types();
+    }
+    CharsMapNormalization(
+        const ov::OutputVector& arguments,
+        const std::shared_ptr<sentencepiece::normalizer::Normalizer> normalizer,
+        const std::shared_ptr<sentencepiece::NormalizerSpec> spec,
+        bool add_dummy_prefix = false,
+        bool remove_extra_whitespaces = false,
+        bool escape_whitespaces = false,
+        bool case_fold = false,
+        const std::string& normalization_form = "",
+        bool nmt = false
+    ): ov::op::Op(arguments), m_normalizer(normalizer), m_spec(spec), m_add_dummy_prefix(add_dummy_prefix), m_remove_extra_whitespaces(remove_extra_whitespaces), m_escape_whitespaces(escape_whitespaces), m_case_fold(case_fold), m_normalization_form(normalization_form), m_nmt(nmt), m_init_flag(){
+        sentencepiece::logging::SetMinLogLevel(1);
+        constructor_validate_and_infer_types();
+    }
+    CharsMapNormalization(
+        const ov::OutputVector& arguments,
+        const std::shared_ptr<sentencepiece::normalizer::Normalizer> normalizer,
+        const std::shared_ptr<sentencepiece::NormalizerSpec> spec,
+        const std::string& normalization_form = ""
+    ): ov::op::Op(arguments), m_normalizer(normalizer), m_spec(spec), m_normalization_form(normalization_form), m_init_flag() {
+        sentencepiece::logging::SetMinLogLevel(1);
         constructor_validate_and_infer_types();
     }
 
     void validate_and_infer_types() override;
 
     std::shared_ptr<ov::Node> clone_with_new_inputs(const ov::OutputVector& inputs) const override {
-        return std::make_shared<CharsMapNormalization>(inputs, m_normalizer, m_spec);
+        return std::make_shared<CharsMapNormalization>(inputs, m_normalizer, m_spec, m_add_dummy_prefix, m_remove_extra_whitespaces, m_escape_whitespaces, m_case_fold, m_normalization_form, m_nmt);
     }
 
     bool visit_attributes(ov::AttributeVisitor& visitor) override {
         visitor.on_attribute("add_dummy_prefix", m_add_dummy_prefix);
+        visitor.on_attribute("remove_extra_whitespaces", m_remove_extra_whitespaces);
         visitor.on_attribute("escape_whitespaces", m_escape_whitespaces);
+        visitor.on_attribute("normalization_form", m_normalization_form);
+        visitor.on_attribute("case_fold", m_case_fold);
+        visitor.on_attribute("nmt", m_nmt);
         return true;
     }
 
@@ -51,9 +79,13 @@ private:
     mutable std::shared_ptr<sentencepiece::normalizer::Normalizer> m_normalizer;
 
     bool m_add_dummy_prefix = false;
+    bool m_remove_extra_whitespaces = true;
     bool m_escape_whitespaces = false;
+    bool m_case_fold = false;
+    bool m_nmt = false;
+    std::string m_normalization_form = "";
 
     // spec should be preserved for the lifetime of the normalizer
     mutable std::shared_ptr<sentencepiece::NormalizerSpec> m_spec;
-    mutable std::mutex m_mutex;
+    mutable std::once_flag m_init_flag;
 };
