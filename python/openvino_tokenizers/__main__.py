46d6831aa68ad2775e61fa68b8e417ed69f0622f
Pavel Esir <pavel.esir@intel.com>
2026-07-27 12:49:58 +0200

    removed redundant files

--- python/openvino_tokenizers/__main__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2018-2026 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-from .cli_tools.main import main
-
-
-if __name__ == "__main__":
-    main()
